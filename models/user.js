4ac84017a6a48acc553f50c12e4cfb1092c16274
Nikhil Kumar <nikhilkumar84993@gmail.com>
2025-01-09 19:35:43 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/miniproject")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  age: Number,
+  email: String,
+  password: String,
+  profilepic: {
+    type: String,
+    default: "default.jpg",
+  },
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+});
+module.exports = mongoose.model("User", userSchema);
